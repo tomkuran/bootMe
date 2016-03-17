dcca81ce27e7e62244c98a75546c5bd261bb6e0c
Tomasz Kuran <tomkuran@gmail.com>
2016-03-17 16:46:28 +0100

    Person Entity

--- src/main/java/pl/tomkuran/domain/Person.java
+++ src/main/java/pl/tomkuran/domain/Person.java
@@ -1,6 +1,6 @@
 package pl.tomkuran.domain;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 /**
  * Created by TKURAN on 3/17/2016.
@@ -8,8 +8,45 @@ import javax.persistence.Entity;
 @Entity
 public class Person {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @Version
+    private Integer version;
+
     private String firstName;
     private String lastName;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
