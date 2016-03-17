c7ed2ea3052ae8322400b70fc664af59d023f175
Tomasz Kuran <tomkuran@gmail.com>
2016-03-17 16:15:24 +0100

    Initial commit

---
+++ src/main/java/pl/tomkuran/BootMeApplication.java
@@ -0,0 +1,12 @@
+package pl.tomkuran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMeApplication.class, args);
+	}
+}
