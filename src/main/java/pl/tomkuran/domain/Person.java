1b8f3d02dec10eef6ece81757f006ffba95cdf69
Tomasz Kuran <tomkuran@gmail.com>
2016-03-17 16:18:50 +0100

    Person Entity

---
+++ src/main/java/pl/tomkuran/domain/Person.java
@@ -0,0 +1,15 @@
+package pl.tomkuran.domain;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by TKURAN on 3/17/2016.
+ */
+@Entity
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+
+}
