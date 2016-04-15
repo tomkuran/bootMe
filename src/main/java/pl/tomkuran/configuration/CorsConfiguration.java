b50b8fbb15c8c5cd5470c513c515b01dc5ee5f0a
Tomasz Kuran <tomkuran@gmail.com>
2016-04-15 15:16:28 +0200

    Cors for /v1/persons/

---
+++ src/main/java/pl/tomkuran/configuration/CorsConfiguration.java
@@ -0,0 +1,25 @@
+package pl.tomkuran.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by TKURAN on 4/14/2016.
+ */
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/v1/persons/").allowedOrigins("null");
+            }
+        };
+    }
+
+}
